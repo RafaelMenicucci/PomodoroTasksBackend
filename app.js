f33f53c91dd8ca4ae53c2f61f3d8d3f9570a2f90
rafael <rafa_alves96@hotmail.com>
2021-08-16 16:17:26 -0300

    fix conections

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ import express from 'express'
 
 const app = express()
 
-const { Pool } = require('pg');
+import pg from 'pg'
+const { Pool } = pg
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
